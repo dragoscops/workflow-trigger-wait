5ec959623864b9872b017810029d738dcf968618
Dragos Cirjan <dragos.cirjan@cellebrite.com>
2024-12-05 23:48:42 +0200

    chore: debug private key as env 1733435322

--- src/github/axios.ts
+++ src/github/axios.ts
@@ -68,6 +68,8 @@ export class GithubAxios {
       privateKey:
         process.env.GITHUB_APP_PRIVATE_KEY || process.env.GH_APP_PRIVATE_KEY || privateKey.replace(/\\n*/g, '\n'),
       request,
+      GITHUB_APP_PRIVATE_KEY: process.env.GITHUB_APP_PRIVATE_KEY,
+      GH_APP_PRIVATE_KEY: process.env.GH_APP_PRIVATE_KEY,
     });
     const auth = createAppAuth({
       appId: Number(appId), // Ensure appId is a number
